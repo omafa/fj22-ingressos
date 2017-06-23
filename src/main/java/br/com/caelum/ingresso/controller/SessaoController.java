6953d3e048a5789e593ae849bf04df33d500284b
Matheus <mafazord@gmail.com>
2017-06-23 18:16:47 -0300

    4.7 concluido, ImdbCliente ok

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -49,8 +49,6 @@ public class SessaoController {
 	public ModelAndView salva(@Valid SessaoForm form, BindingResult result){
 		if (result.hasErrors()) return form(form.getSalaId(),form);
 		
-		//ModelAndView modelAndView = new ModelAndView("redirect:/admin/sala/"+form.getSalaId()+"/sessoes");
-		
 		Sessao sessao = form.toSessao(salaDao, filmeDao);
 		
 		List<Sessao> sessoesDaSala = sessaoDao.buscaSessoesDaSala(sessao.getSala());
--- src/main/java/br/com/caelum/ingresso/rest/ImdbClient.java
+++ src/main/java/br/com/caelum/ingresso/rest/ImdbClient.java
@@ -3,21 +3,22 @@ package br.com.caelum.ingresso.rest;
 import java.util.Optional;
 
 import org.apache.log4j.Logger;
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.modelo.DetalhesDoFilme;
 
-@Controller
+@Component
 public class ImdbClient {
 	
 	private	Logger	logger	=	Logger.getLogger(ImdbClient.class);
 	
 	
-	public	Optional<DetalhesDoFilme>request(Filme	filme){
-					RestTemplate	client	=	new	RestTemplate();
+	public	Optional<DetalhesDoFilme> request(Filme	filme){
+		
+					RestTemplate client	=	new	RestTemplate();
 					String	titulo	=	filme.getNome().replace("	",	"+");
 					String	url	=	String.format("https://imdb-fj22.herokuapp.com/imdb?title=%s",	titulo);
 					try	{
